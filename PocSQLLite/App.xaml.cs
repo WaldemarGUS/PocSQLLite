b5a4698d675c536156c949a9f7ac2d845a8fc46b
Waldemar Asmus <waldemar.asmus@gus-group.com>
2025-04-25 09:53:06 +0200

    init

---
+++ PocSQLLite/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PocSQLLite
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PocSQLLite/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PocSQLLite
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
