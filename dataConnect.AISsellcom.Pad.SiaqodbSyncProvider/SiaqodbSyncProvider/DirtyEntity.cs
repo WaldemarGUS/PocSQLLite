ac9aaa0299e4c1cd7d70b4b0a9fcf2a139861347
Waldemar Asmus <waldemar.asmus@gus-group.com>
2025-04-30 18:27:16 +0200

    Refactor DirtyEntity class: rename Id to OID, add EntityOID property, and update attributes for consistency in SiaqodbSyncProvider.

--- dataConnect.AISsellcom.Pad.SiaqodbSyncProvider/SiaqodbSyncProvider/DirtyEntity.cs
+++ dataConnect.AISsellcom.Pad.SiaqodbSyncProvider/SiaqodbSyncProvider/DirtyEntity.cs
@@ -4,27 +4,25 @@
 // MVID: 45756F40-2044-4D99-BD9E-C1D39F89AA18
 // Assembly location: \\Mac\Home\Downloads\SiaqodbSyncFW\bin\Siaqodb_5x\SiaqodbSyncProvider.dll
 
-using Sqo.Attributes;
 using System.Reflection;
 using SQLite;
 
-namespace SiaqodbSyncProvider {
-    [Obfuscation(Exclude = true)]
-    public class DirtyEntity
+namespace SiaqodbSyncProvider
+{
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    internal class DirtyEntity
     {
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        public int OID { get; set; }
+
+        public int EntityOID { get; set; }
         
-        [SQLite.MaxLength(200)]
+        [MaxLength(200)]
         public string EntityType { get; set; }
-        
-        public int EntityOID;
         
         public DirtyOperation DirtyOp { get; set; }
         
         public byte[] TombstoneObj { get; set; }
-
-        public int OID { get; set; }
     }
 }
 
